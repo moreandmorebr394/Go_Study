f5864c00ea8e988c580a45af2d41cb6c04fd70fd
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2024-01-11 14:03:06 -0300

    Create Study 2.go

---
+++ Study 2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func doubleInteger(num int) int {
+    return num * 2
+}
+
+func main() {
+    // Example usage:
+    result := doubleInteger(5)
+    fmt.Println(result) // Output: 10
+}
